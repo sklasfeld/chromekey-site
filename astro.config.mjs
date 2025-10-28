9011e83ac68799e7d8ec868125d2cbaa78c27db5
sklasfeld <sjk314@gmail.com>
2025-10-28 09:07:25 -0400

    Update site URL to chromekey.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://chromekey.netlify.app",
   integrations: [mdx(), sitemap(), icon()],
 
   vite: {
